453f38ab434e421ce5e83d1e4a4de4c69a276442
pawelrudnicki <kontakt.prudnicki@gmail.com>
2021-12-20 12:32:53 +0100

    task functional

---
+++ src/functional-concepts/functional-programming.js
@@ -0,0 +1,28 @@
+const users = [
+    {
+        name: "Janusz",
+        age: 41,
+        profession: "Hydraulik"
+    },
+    {
+        name: "Kazimiera",
+        age: 25,
+        profession: "Senior HR"
+    },
+    {
+        name: "Barbara",
+        age: 35,
+        profession: "Programistka"
+    },
+    {
+        name: "Zbyszek",
+        age: 55,
+        prefession: "Kierowca"
+    }
+];
+
+/*
+Przekształcić podaną tablice użytkowników na tablice imion tylko dla kobiet pisanymi drukowanymi literami,
+używali tych funkcji 
+tj. ['KAZIMIERA', 'BARBARA']
+ */

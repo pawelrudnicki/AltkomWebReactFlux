a9034ee53e84ba1449c16b7a65b9a641218227e6
pawelrudnicki <kontakt.prudnicki@gmail.com>
2021-12-20 12:00:30 +0100

    task2

--- src/functional-concepts/muttable-immutable-operations.js
+++ src/functional-concepts/muttable-immutable-operations.js
@@ -2,7 +2,15 @@
 // const otherNumbers = numbers;
 // numbers.push(5);
 
-const numbers = [1, 2, 3, 4];
-const otherNumbers = [];
+// const numbers = [1, 2, 3, 4];
+//
+// // #1 Zadanie - jak utworzyc nowa tablice a nie mutowac pozostalej?
+// const otherNumbers = [...numbers];
+// otherNumbers.push(5);
+// console.log(numbers);
+// console.log(otherNumbers);
 
-// #1 Zadanie - jak utworzyc nowa tablice a nie mutowac pozostalej?
+const letters = ['a', 'b', 'c'];
+// #2 Zadanie: Jak utworzyc tablice ze zmofyfikowanymi elementami
+const myUpperLetters = [];
+

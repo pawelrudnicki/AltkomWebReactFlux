81a26f9e65f7497123a1932663c2298ff43917fb
pawelrudnicki <kontakt.prudnicki@gmail.com>
2021-12-20 11:52:06 +0100

    task 1

---
+++ src/functional-concepts/muttable-immutable-operations.js
@@ -0,0 +1,8 @@
+// const numbers = [1, 2, 3, 4];
+// const otherNumbers = numbers;
+// numbers.push(5);
+
+const numbers = [1, 2, 3, 4];
+const otherNumbers = [];
+
+// #1 Zadanie - jak utworzyc nowa tablice a nie mutowac pozostalej?
